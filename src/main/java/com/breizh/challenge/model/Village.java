fd7ce6d5513f565bf5cc7f34a5951d96d3c80c33
Alexandre Blache <alexandreblache.pro@gmail.com>
2020-12-21 10:46:41 +0100

    village table

---
+++ src/main/java/com/breizh/challenge/model/Village.java
@@ -0,0 +1,25 @@
+package com.breizh.challenge.model;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public @Data class Village {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    String name;
+    int postCode;
+
+
+
+}
